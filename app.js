3c2fdfbd60a63b24786aa07eed28518ed800e5e8
SmrutiChandan <85742019+SmrutiChandan@users.noreply.github.com>
2023-10-16 00:40:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.end('Hello, Cisco team, Demo of test job successful.Now please ask your queries now!\n');
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Welcome Team Cisco. Demo deployment is successful & Server running at http://${hostname}:${port}/`);
+  console.log(`log`);
 });
