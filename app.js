a0e2fd6ff765105a7e0b1e5ed266cef26f77d8ef
SmrutiChandan <85742019+SmrutiChandan@users.noreply.github.com>
2023-10-16 15:05:53 +0530

    Update1 app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Cisco team, Demo of test job successful.Now please ask your queries now!\n');
+  res.end('Hello, Cisco team \n');
 });
 
 server.listen(port, hostname, () => {
